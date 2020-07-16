a882ef9ff818b8b8f124dd85d9357bb1554e2806
josekgobJHB <josekgob@student.wethinkcode.co.za>
2020-07-16 12:26:07 +0200

    exercise 01

--- ex01/ft_recursive_factorial.c
+++ ex01/ft_recursive_factorial.c
@@ -6,39 +6,22 @@
 /*   By: josekgob <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 17:38:58 by josekgob          #+#    #+#             */
-/*   Updated: 2020/07/16 10:50:56 by josekgob         ###   ########.fr       */
+/*   Updated: 2020/07/16 12:22:16 by josekgob         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
-#include <stdio.h>
-
 
 int ft_recursive_factorial(int nb)
 {
-                int		factorial;
-
-				factorial = 1;
-                if(nb == 0 || nb == 1)
+				if (nb == 0)
                 {
-                                return (factorial);
+                                return (1);
                 }
-				else if(nb > 1)
+				if(nb >= 1)
 				{
-								while(nb > 1)
-								{
-												factorial = nb *(nb--);
-												nb--;
-												if(nb == 1)
-												{
-																return (factorial);
-												}
-	   							}
+								return (nb * ft_recursive_factorial(nb - 1));
 				}
-				
-				return (0);
-}
-
-int main(void)
-{
-				printf("%d", ft_recursive_factorial(4));
+				else
+				{
+								return (0);
+				}
 }
